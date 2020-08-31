889885cc201fdf2213dfb66695e099f435a417af
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-01 00:09:57 +0500

    Add methods to print and update board

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Board
+    def initialize
+        @game_board = {'1' => '1', '2' => '2', '3' => '3',
+              '4' => '4', '5' => '5', '6' => '6',
+              '7' => '7', '8' => '8', '9' => '9'
+             }
+    end
+
+    def print_board
+    end
+
+    def update_board
+    end
+
+end
