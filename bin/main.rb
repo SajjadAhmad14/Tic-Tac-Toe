2c3cd140d6295db9711aa414eced834bf880c982
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-01 00:12:16 +0500

    main exectuable file now show validated names of players

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
-
-game_board = {'1' => '1', '2' => '2', '3' => '3',
-              '4' => '4', '5' => '5', '6' => '6',
-              '7' => '7', '8' => '8', '9' => '9'
-             }
+require_relative '../lib/player'
 
 # Player names should contain at least one letter
 puts "Please enter player1 name!"
 player1_name = gets.chomp
-
+player1_name = Player.validate_names(player1_name)
+puts "#{player1_name}"
 puts "Please enter player2 name!"
 player2_name = gets.chomp
+player2_name = Player.validate_names(player2_name)
+puts "#{player2_name}"
+player = Player.new(player1_name, player2_name)
 
 puts "Welcome to Tic Tac Toe, #{player1_name} and #{player2_name}"
 
@@ -21,13 +21,13 @@ puts
 print game_board['7'] + " " + "|" + game_board['8'] + " " + "|" + " " + game_board['9']
 puts
 
-#Player move should only be 1-9 and if place is already filled it should reprompt the player
-puts "It's #{player1_name} turn now!"
-puts "Please enter 1-9 to put x there"
-location = gets.chomp
-puts game_board[location]
+# #Player move should only be 1-9 and if place is already filled it should reprompt the player
+# puts "It's #{player1_name} turn now!"
+# puts "Please enter 1-9 to put x there"
+# location = gets.chomp
+# puts game_board[location]
 
-puts "It's #{player2_name} turn now!"
-puts "Please enter 1-9 to put x there"
-location = gets.chomp
-puts game_board[location]
+# puts "It's #{player2_name} turn now!"
+# puts "Please enter 1-9 to put x there"
+# location = gets.chomp
+# puts game_board[location]
