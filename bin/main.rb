fb69dc2cbe7f30dac5a162f2a424cbea199723f7
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-03 14:15:01 +0500

    Add a method in Board class which validates user move

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../lib/player'
+require_relative '../lib/board'
 
-# Player names should contain at least one letter
 puts "Please enter player1 name!"
 player1_name = gets.chomp
 player1_name = Player.validate_names(player1_name)
@@ -13,21 +13,12 @@ puts "#{player2_name}"
 player = Player.new(player1_name, player2_name)
 
 puts "Welcome to Tic Tac Toe, #{player1_name} and #{player2_name}"
+board = Board.new
+board.print_board
 
-print game_board['1'] + " " + "|" + game_board['2'] + " " + "|" + " " + game_board['3']
-puts
-print game_board['4'] + " " + "|" + game_board['5'] + " " + "|" + " " + game_board['6']
-puts 
-print game_board['7'] + " " + "|" + game_board['8'] + " " + "|" + " " + game_board['9']
-puts
+# Player move should only be 1-9 and if place is already filled it should reprompt the player
+puts "Please enter number 1-9 to put x or o there!"
+move = gets.chomp
+board.validate_move(move)
 
-# #Player move should only be 1-9 and if place is already filled it should reprompt the player
-# puts "It's #{player1_name} turn now!"
-# puts "Please enter 1-9 to put x there"
-# location = gets.chomp
-# puts game_board[location]
 
-# puts "It's #{player2_name} turn now!"
-# puts "Please enter 1-9 to put x there"
-# location = gets.chomp
-# puts game_board[location]
--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,26 @@
 class Board
     def initialize
-        @game_board = {'1' => '1', '2' => '2', '3' => '3',
+        @game_board = {'1' => '1', '2' => '', '3' => '3',
               '4' => '4', '5' => '5', '6' => '6',
               '7' => '7', '8' => '8', '9' => '9'
              }
     end
 
     def print_board
+      print @game_board['1'] + " " + "|" + @game_board['2'] + " " + "|" + " " + @game_board['3']
+      puts
+      print @game_board['4'] + " " + "|" + @game_board['5'] + " " + "|" + " " + @game_board['6']
+      puts 
+      print @game_board['7'] + " " + "|" + @game_board['8'] + " " + "|" + " " + @game_board['9']
+      puts
+    end
+
+    def validate_move(move)
+      until (move.to_i >=1 && move.to_i <= 9) && (@game_board[move] == '')
+        puts "Please enter your move between 1-9 only"
+        move = gets.chomp
+      end
+      move
     end
 
     def update_board
