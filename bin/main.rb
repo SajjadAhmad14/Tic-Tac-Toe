08c839f52a12dfa071fea8ba49ede0ba7ba714f7
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-04 17:14:52 +0500

    Add draw method to board

--- bin/main.rb
+++ bin/main.rb
@@ -15,12 +15,14 @@ player = Player.new(player1_name, player2_name)
 puts "Welcome to Tic Tac Toe, #{player1_name} and #{player2_name}"
 board = Board.new(player)
 board.print_board
-board.reset_board
-board.print_board
-
 loop do
   puts "#{player.name1}, it's you turn! Please enter in 1-9 in blank location only"
   move = gets.chomp
   move = board.validate_move(move)
   board.update_board(move, player1_name)
+  puts "#{player.name2}, it's you turn! Please enter in 1-9 in blank location only"
+  move = gets.chomp
+  move = board.validate_move(move)
+  board.update_board(move, player2_name)
+  break if board.draw == true
 end
--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,22 @@
 class Board
-    attr_accessor :game_board
+    attr_accessor :game_board, :draw
     def initialize(player)
-        @game_board = {'1' => '1', '2' => '3', '3' => '3',
-              '4' => '4', '5' => '5', '6' => '6',
-              '7' => '7', '8' => '8', '9' => '9'
+        @game_board = {'1' => '', '2' => '', '3' => '',
+              '4' => '', '5' => '', '6' => '',
+              '7' => '', '8' => '', '9' => ''
              }
         @player = player
+        @win = ''
+        @draw = false
     end
 
     def print_board
-    #   print @game_board['1'] + " " + "|" + " " + @game_board['2'] + "|" + @game_board['3']
-    #   puts
-    #   print @game_board['4'] + " " + "|" + " " + @game_board['5'] + "|" + @game_board['6']
-    #   puts 
-    #   print @game_board['7'] + " " + "|" + " " + @game_board['8'] + "|" + @game_board['9']
-    #   puts
-    puts @game_board
+      print @game_board['1'] + '|' + @game_board['2'] + '|' + @game_board['3']
+      puts
+      print @game_board['4'] + '|' + @game_board['5'] + '|' + @game_board['6']
+      puts 
+      print @game_board['7'] + '|' + @game_board['8'] + '|' + @game_board['9']
+      puts
     end
 
     def validate_move(move)
@@ -40,30 +41,41 @@ class Board
         end
         print_board
         check_vertical
-        check_horizontal
-        check_diagonal
+       check_horizontal
+       check_diagonal
+       draw
+        
     end
-    
+
     def check_vertical
-        vertical1 = game_board['1'], game_board['2'], game_board['7']
+        vertical1 = game_board['1'], game_board['4'], game_board['7']
         vertical2 = game_board['2'], game_board['5'], game_board['8']
         vertical3 = game_board['3'], game_board['6'], game_board['9']
-        puts "#{@player.name1} won" if vertical1.all?('x') || vertical2.all?('x') || vertical3.all?('x')
-        puts "#{@player.name2} won" if vertical1.all?('o') || vertical2.all?('o') || vertical3.all?('o')
+        puts "player1 wins" if vertical1.all?('x') || vertical2.all?('x') || vertical3.all?('x')
+        puts "player2 wins" if vertical1.all?('o') || vertical2.all?('o') || vertical3.all?('o')
     end
 
     def check_horizontal
         horizontal1 = game_board['1'], game_board['2'], game_board['3']
         horizontal2 = game_board['4'], game_board['5'], game_board['6']
         horizontal3 = game_board['7'], game_board['8'], game_board['9']
-        puts "#{@player.name1} won" if horizontal1.all?('x') || horizontal2.all?('x') || horizontal3.all?('x')
-        puts "#{@player.name2} won" if horizontal1.all?('o') || horizontal2.all?('o') || horizontal3.all?('o')
+        puts "player1 wins" if horizontal1.all?('x') || horizontal2.all?('x') || horizontal3.all?('x')
+        puts "player2 wins" if horizontal1.all?('o') || horizontal2.all?('o') || horizontal3.all?('o')
     end
 
     def check_diagonal
       diagonal1 = game_board['1'], game_board['5'], game_board['9']
       diagonal2 = game_board['3'], game_board['5'], game_board['7']
-      puts "#{@player.name1} won" if diagonal1.all?('x') || diagonal2.all?('x')
-      puts "#{@player.name2} won" if diagonal1.all?('o') || diagonal2.all?('o')
-    end 
+      puts "player1 wins" if diagonal1.all?('x') || diagonal2.all?('x')
+      puts "player2 wins" if diagonal1.all?('o') || diagonal2.all?('o')
+    end
+
+    def draw
+      if @game_board.all? {|key,value| value != ''}
+        puts "It's a draw" 
+        @draw = true
+      else
+        @draw = false
+      end
+    end
 end
